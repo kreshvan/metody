3e9d843dd1daa5a1cf364acb71908f2a118d7e47
днс <kreshvan12317@yandex.ru>
2024-06-09 22:16:49 +0300

    homework metody

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,45 @@
 public class Main {
+    //задание 1
+    public static void printIssueCount(int number) {
+        if (number > 1584 && number % 4 == 0 && number % 100 != 0 || (number > 1584 && number % 400 == 0)) {
+            System.out.println(number + " " + "год високосный");
+        } else {
+            System.out.println(number + " " + "не год високосный");
+        }
+    }
+    //задание 2
+    public static void phoneYear(int phones, int year) {
+        if (phones == 1 && year <= 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if (phones == 1 && year > 2015)
+            System.out.println("Установите версию приложения для Android по ссылке");
+        if (phones == 0 && year > 2015) {
+            System.out.println("Установите версию приложения для IOS по ссылке.");
+        } else if (phones == 0 && year <= 2015)
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+    }
+    //задание 3
+    public static void distanceTime(int km) {
+        if (km <= 20) {
+            System.out.println("Потребуется дней:1" + " " + "срок доставки.");
+        } else if (km > 20 && km < 60) {
+            System.out.println("Потребуется дней:2" + " " + "срок доставки.");
+        } else if (km >= 60 && km < 100) {
+            System.out.println("Потребуется дней:3" + " " + "срок доставки.");
+        } else if (km >= 100) {
+            System.out.println("доставки нет.");
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int year = 40001;
+        printIssueCount(year);
+        int deliveryDistance = 50;
+        distanceTime(deliveryDistance);
+        int namePhone = 0;
+        int clientDeviceYear = 2222;
+        phoneYear(namePhone,clientDeviceYear);
     }
 }
+
+
+
