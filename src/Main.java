5a275c454ace31facd33c940ce09cee69de9fb87
днс <kreshvan12317@yandex.ru>
2024-06-10 09:55:30 +0300

    homework metody korrection

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,24 @@
 public class Main {
     //задание 1
-    public static void printIssueCount(int number) {
+    public static void checkLeapYear(int number) {
         if (number > 1584 && number % 4 == 0 && number % 100 != 0 || (number > 1584 && number % 400 == 0)) {
-            System.out.println(number + " " + "год високосный");
+            System.out.println(number + " "+"год-високосный год.");
         } else {
-            System.out.println(number + " " + "не год високосный");
+            System.out.println(number + " "+"год-не високосный.");
         }
     }
     //задание 2
-    public static void phoneYear(int phones, int year) {
+    public static void suggestAppVersion (int phones, int year) {
         if (phones == 1 && year <= 2015) {
             System.out.println("Установите облегченную версию приложения для Android по ссылке");
-        } else if (phones == 1 && year > 2015)
+        } else if (phones == 1 && year > 2015){
             System.out.println("Установите версию приложения для Android по ссылке");
+        }
         if (phones == 0 && year > 2015) {
             System.out.println("Установите версию приложения для IOS по ссылке.");
-        } else if (phones == 0 && year <= 2015)
+        } else if (phones == 0 && year <= 2015) {
             System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        }
     }
     //задание 3
     public static void distanceTime(int km) {
@@ -32,12 +34,12 @@ public class Main {
     }
     public static void main(String[] args) {
         int year = 40001;
-        printIssueCount(year);
+        checkLeapYear(year);
         int deliveryDistance = 50;
         distanceTime(deliveryDistance);
         int namePhone = 0;
         int clientDeviceYear = 2222;
-        phoneYear(namePhone,clientDeviceYear);
+        suggestAppVersion (namePhone,clientDeviceYear);
     }
 }
 
